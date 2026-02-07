24da60b6d63947bad589f89caf40684e31da177c
Rodrigo Thenopholo <thenopholo92@gmail.com>
2026-02-07 19:32:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900">My Money</h1>
+        <p className="mt-2 text-gray-600">Gerenciador financeiro pessoal</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
